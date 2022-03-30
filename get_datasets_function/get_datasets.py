34e39ab4273ec879db62e83f2689c6af834a9c32
Justin Wu <justin4827@gmail.com>
2022-03-30 00:15:29 -0400

    Renamed generate function files & directories

---
+++ get_datasets_function/get_datasets.py
@@ -0,0 +1,26 @@
+import json
+import os
+import boto3
+
+def lambda_handler(event, context):
+    try:
+        bucket_name = os.environ['AWS_BUCKET_NAME']
+        s3 = boto3.client('s3')
+        bucket = s3.list_objects(Bucket=bucket_name, Delimiter='/')
+
+        prefixes = [prefix_obj.get('Prefix')[:-1] for prefix_obj in bucket.get('CommonPrefixes')]
+    except:
+        print('Failed to get datasets from S3')
+        return {
+            'statusCode': 500,
+            'body': 'Failed to get datasets'
+        }
+
+    print(prefixes)
+    return {
+        'statusCode': 200,
+        'body': prefixes
+    }
+
+if __name__=='__main__':
+    lambda_handler(0,0)
